d0cb7fa385d75483cdec7bcbcb22ee5d1b2995aa
Paulo Henrique <p.henriquerm@hotmail.com>
2023-11-23 15:59:02 -0300

    200 Buscando dados externos com fetch (Data fetching)

--- src/App.js
+++ src/App.js
@@ -3,75 +3,51 @@ import { Component } from 'react';
 
 class App extends Component {
   state = { 
-    counter: 0,
-    posts: [
-      { 
-        id: 1,
-        title: 'O titulo 1',
-        body: 'O corpo 1',
-      },
-      { 
-        id: 2,
-        title: 'O titulo 2',
-        body: 'O corpo 2',
-      },
-      { 
-        id: 3,
-        title: 'O titulo 3',
-        body: 'O corpo 3',
-      },
-    ]
+    posts: []
   };
 
   timeoutUpdate = null;
 
   componentDidMount() {
-    this.handleTimeout();
+    this.loadPosts();
   }
 
-  componentDidUpdate() {
-    // this.handleTimeout();
-  }
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+    
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
 
-  componentWillUnmount() {
-    clearTimeout(this.timeoutUpdate)
-  }
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url }
+    })
 
-  handleTimeout = () => {
-    const { posts, counter } = this.state;
-    posts[0].title = 'titulo mudou';
-    this.timeoutUpdate = setTimeout(() => {
-      this.setState({ posts, counter: counter + 1 });
-    }, 2000);
+    this.setState({ posts: postsAndPhotos });
   }
 
   render() {
-    const { posts, counter } = this.state;
+    const { posts } = this.state;
   
     return (
-      <div className="App">
-        <h1>{counter}</h1>
-        {
-          posts.map(post => (
-            <div key={post.id}>
-              <h1>{post.title}</h1> 
-              <p>{post.body}</p> 
-            </div>
-          ))
-        }
-      </div>
+      <section className='container'>
+        <div className='posts'>
+          {
+            posts.map(post => (
+              <div className='post'>
+                <img src={post.cover} alt={post.title} />     
+                <div key={post.id} className='post-content'>
+                  <h1>{post.title}</h1> 
+                  <p>{post.body}</p> 
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </section>
     );
   }
-
-   // handlePClick = () => {
-  //   this.setState({ name: 'Júnior' });
-  // }
-
-  // handleAClick = (event) => {
-  //   event.preventDefault();
-  //   const { counter } = this.state;
-  //   this.setState({ counter: counter + 1 })
-  // }
 }
 
 
